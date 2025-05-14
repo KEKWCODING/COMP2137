e05b47343928c091df0c070ca31dd32900ec46bf
Kaden McTavish <kmct123@hotmail.com>
2025-05-14 15:29:40 -0400

    Lab 1 Demos Completed

---
+++ helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script created to demonstrate scripting process
+#this script will print out Hello World!
+#It does nothing else, and uses no input
+
+#Step 1: print the text
+
+echo "Hello World"
